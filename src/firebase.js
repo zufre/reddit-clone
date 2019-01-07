301e542b7cdcdcbefbde64bd0b10cc6f0f834710
Cédric David <cedric.david@btopenworld.com>
2019-01-07 16:20:26 +1000

    update files

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+
+import firebase from 'firebase';
+  var config = {
+    apiKey: "AIzaSyAIBVEFIGO7VQQHbMnbeJ8A1qmoZQNBSVU",
+    authDomain: "redditclone-e7293.firebaseapp.com",
+    databaseURL: "https://redditclone-e7293.firebaseio.com",
+    projectId: "redditclone-e7293",
+    storageBucket: "redditclone-e7293.appspot.com",
+    messagingSenderId: "860270189553"
+  };
+  firebase.initializeApp(config);
